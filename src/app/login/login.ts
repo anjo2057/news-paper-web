d05943d91361ef4c72bd05c4c3a3994f9684e247
anjo2057 <ajohanssond76@gmail.com>
2025-10-10 11:50:12 +0200

    feat: more on login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { User } from '../interfaces/user';
+import { error } from 'console';
+import { BlobOptions } from 'buffer';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
+  constructor(private loginservice: LoginService) {}
 
+  private user: User | null = null;
+
+  login(): void {
+    if (this.user) {
+      this.loginservice.login(this.user.username, this.user.passwd);
+    }
+  }
+
+  checkLogin(): boolean {
+    if (this.loginservice.isLogged()) {
+      return this.loginservice.isLogged();
+    } else {
+      window.alert(
+        'The user: ' + this.user?.username + 'could not be logged in. Wrong password or username'
+      );
+      return false;
+    }
+  }
 }
