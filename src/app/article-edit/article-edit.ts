fa81ba38c1e8ed6897b889a724d774e0e4ba5241
anjo2057 <ajohanssond76@gmail.com>
2025-10-15 13:56:22 +0200

    feat: added full editing of an article

--- src/app/article-edit/article-edit.ts
+++ src/app/article-edit/article-edit.ts
@@ -3,18 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { HighlightDirective } from '../directives/highlight.directive';
 import { Article } from '../interfaces/article';
 import { CommonModule, Location } from '@angular/common';
-import { NewsService } from '../services/news.service'
-import { EditorModuleModule } from '../editor-module/editor-module-module';
+import { NewsService } from '../services/news.service';
 import { AngularEditorModule } from '@kolkov/angular-editor';
-
-
-
-
-
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-article-edit',
-  imports: [FormsModule, HighlightDirective, CommonModule, EditorModuleModule, AngularEditorModule],
+  imports: [FormsModule, HighlightDirective, CommonModule, AngularEditorModule],
   templateUrl: './article-edit.html',
   styleUrl: './article-edit.css',
 })
@@ -22,6 +17,54 @@ export class ArticleEdit implements OnInit {
   @ViewChild('articleForm') articleForm: any;
 
   constructor(private newsService: NewsService, private Location: Location) {}
+
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: 'auto',
+    minHeight: '0',
+    maxHeight: 'auto',
+    width: 'auto',
+    minWidth: '0',
+    translate: 'yes',
+    enableToolbar: true,
+    showToolbar: true,
+    placeholder: 'Enter text here...',
+    defaultParagraphSeparator: '',
+    defaultFontName: '',
+    defaultFontSize: '',
+    fonts: [
+      { class: 'arial', name: 'Arial' },
+      { class: 'times-new-roman', name: 'Times New Roman' },
+      { class: 'calibri', name: 'Calibri' },
+      { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+    ],
+    customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText',
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+    uploadUrl: 'v1/image',
+    sanitize: true,
+    toolbarHiddenButtons: [
+      ['bold', 'italic'],
+      ['fontSize'],
+      // ['insertImage', 'insertVideo', 'insertHorizontalRule', 'removeFormat', 'toggleEditorMode'],
+      // ['link', 'unlink', 'superscript', 'subscript'],
+      // ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'indent', 'outdent'],
+      // ['cut', 'copy', 'delete', 'undo', 'redo'],
+    ],
+  };
 
   article: Article = {
     id: 0, //TODO : might not needed check here and in interfaces
@@ -50,6 +93,20 @@ export class ArticleEdit implements OnInit {
 
   clear(): void {
     this.articleForm.reset();
+  }
+
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.article.image_data = reader.result as string;
+        this.article.image_media_type = file.type;
+        this.article.thumbnail_image = reader.result as string;
+        this.article.thumbnail_media_type = file.type;
+      };
+      reader.readAsDataURL(file);
+    }
   }
 
   goBack(): void {
