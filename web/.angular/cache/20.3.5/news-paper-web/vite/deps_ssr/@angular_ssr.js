276f9e595fc139ce604c8bd4464ae8ab86ddf76b
noraangeus <nora.angeus@gmail.com>
2025-10-28 14:11:11 +0100

    moved web app into web folder

---
+++ web/.angular/cache/20.3.5/news-paper-web/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,43 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-4CCOU65F.js";
+import "./chunk-WFMQ4VOS.js";
+import "./chunk-KHTNMBTG.js";
+import "./chunk-6HA7YFJE.js";
+import "./chunk-DFMGQN4V.js";
+import "./chunk-Y7IVMKPC.js";
+import "./chunk-3B2B543A.js";
+import "./chunk-7SULSMEY.js";
+import "./chunk-JRBTNWFI.js";
+import "./chunk-W6MIQTXE.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
